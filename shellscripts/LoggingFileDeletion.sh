6fae03ceaf05853b130a09f374c9731c8171d9d2
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-01 22:19:19 +0000

    Update and rename LogginFileDeletion.sh to LoggingFileDeletion.sh

--- shellscripts/LoggingFileDeletion.sh
+++ shellscripts/LoggingFileDeletion.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # This script logs all file deletion requests
+# Usage
+# -----
+# Option 1
+# Save the script as a local file - eg: logrm.sh
+# alias rm=logrm
+#
+# Option 2
+# rename /bin/rm to /bin/rm.old (change line 27 to match this)
+# save this file as /bin/rm
+
 
 removelog="/var/log/remove.log"
 
@@ -13,7 +23,7 @@ fi
 # Records the user action to the logfile
 echo "$(date): ${USER}: $@" >> $removelog
 
-# Passes the request to the real rm program
+# Passes the request to the real rm program - note if you have renamed it (option 2 above) you must change this line
 /bin/rm "$@
 
 exit 0
