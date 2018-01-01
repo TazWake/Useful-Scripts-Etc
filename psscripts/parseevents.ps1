dcf601370aeb593338361156dd38b4bd414fb404
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-01 23:13:12 +0000

    Update parseevents.ps1

--- psscripts/parseevents.ps1
+++ psscripts/parseevents.ps1
@@ -1,2 +1,4 @@
 # quick review of evtx for obvious badness
-Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4624,4648,4672}
+Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4624}` | Select-Object -Property timecreated,id, @{label='username';expression={$_.properties[5].value}}, @{label='domain';expression={$_.properties[6].value}}, @{label='Source';expression={$_.properties[18].value}} | export-csv review_events_4624.csv
+Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4648}` | Select-Object -Property timecreated,id, @{label='username';expression={$_.properties[5].value}}, @{label='domain';expression={$_.properties[6].value}}, @{label='Source';expression={$_.properties[12].value}} | export-csv review_events_4648.csv
+Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4672}` | Select-Object -Property timecreated,id, @{label='username';expression={$_.properties[1].value}},@{label='Domain';expression={$_.properties[2].value}} | export-csv review_events_4672.csv
