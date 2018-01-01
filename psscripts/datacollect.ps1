ca1e3abcaa971888c72c429061f563fef0cdbbf6
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-01 22:51:57 +0000

    Create datacollect.ps1

---
+++ psscripts/datacollect.ps1
@@ -0,0 +1,28 @@
+# This is a simple script to collect IR data
+
+Write-host "[+} Commencing data collection..." - foregroundcolor white
+date | out-file -append ".\hash.txt"
+
+# Collect general information
+Systeminfo | out-file -append ".\results.txt"
+
+#Networking
+Ipconfig /all | out-file -append ".\results.txt"
+
+#Local Users
+Net user | out-file -append ".\results.txt"
+
+#Networking
+Netstat -nao | out-file -append ".\results.txt"
+
+#Process list from pstree
+.\pstree.ps1
+
+#Journal Entry
+Wevtutil epl security .\security.evtx
+
+#Hash Results
+Get-FileHash ".\results.txt" | Format-List | out-file -append ".\hash.txt"
+Get-FileHash ".\security.evtx" | Format-List | out-file -append ".\hash.txt"
+
+Write-host "[+] Collection over" -foregroundcolor white
