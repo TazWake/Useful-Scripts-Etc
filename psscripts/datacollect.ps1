4535a92b034b597e2921fff713d660740217d56a
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-01 22:56:07 +0000

    Update datacollect.ps1

--- psscripts/datacollect.ps1
+++ psscripts/datacollect.ps1
@@ -7,20 +7,20 @@ date | out-file -append ".\hash.txt"
 # Collect general information
 Systeminfo | out-file -append ".\results.txt"
 
-#Networking
-Ipconfig /all | out-file -append ".\results.txt"
+# Networking
+ipconfig /all | out-file -append ".\results.txt"
 
-#Local Users
-Net user | out-file -append ".\results.txt"
+# Local Users
+net user | out-file -append ".\results.txt"
 
 #Networking
-Netstat -nao | out-file -append ".\results.txt"
+netstat -nao | out-file -append ".\results.txt"
 
 #Process list from pstree
 .\pstree.ps1
 
 #Journal Entry
-Wevtutil epl security .\security.evtx
+wevtutil epl security .\security.evtx
 
 #Hash Results
 Get-FileHash ".\results.txt" | Format-List | out-file -append ".\hash.txt"
