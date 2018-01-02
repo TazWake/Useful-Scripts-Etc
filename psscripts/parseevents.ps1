2e9ef954cbf62daaa644944017f390289385374e
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-02 07:28:28 +0000

    Update parseevents.ps1

--- psscripts/parseevents.ps1
+++ psscripts/parseevents.ps1
@@ -1,3 +1,6 @@
+# Grab the Security Event Log
+wevtutil epl security .\security.evtx
+
 # quick review of evtx for obvious badness
 Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4624}` | Select-Object -Property timecreated,id, @{label='username';expression={$_.properties[5].value}}, @{label='domain';expression={$_.properties[6].value}}, @{label='Source';expression={$_.properties[18].value}} | export-csv review_events_4624.csv
 Get-WinEvent -FilterHashtable @{path='.\security.evtx';id=4648}` | Select-Object -Property timecreated,id, @{label='username';expression={$_.properties[5].value}}, @{label='domain';expression={$_.properties[6].value}}, @{label='Source';expression={$_.properties[12].value}} | export-csv review_events_4648.csv
