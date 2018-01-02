a54c6649689f94c8ea0b3a855725f7e37036b11b
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-02 08:47:25 +0000

    Create userreport.sh

---
+++ shellscripts/userreport.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This is a quick script to gather some key data on a system. It is not a forensic script.
+# More entries will be added over time.
+
+echo "Data Collection Report for $USER"
+echo "================================"
+
+echo "screen resolution"
+echo xdpyinfo | grep dimensions
+
+echo "Network Config"
+ifconfig
+
+echo "OS version"
+uname -a
+
+echo "Public IP"
+curl icanhazip.com
+
+echo "Generating Process List"
+ps -al > ./pslist.txt
+
+echo "================================"
