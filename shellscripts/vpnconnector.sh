b0b02ad07d0c0af0c1092b684fa6d72a6a96d45f
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-05 11:20:33 +0000

    Create vpnconnector.sh

---
+++ shellscripts/vpnconnector.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+    then
+        echo "Usage: arbitraryvpn <path>"
+        exit
+fi
+
+FILES_ARRAY=(`ls $1`)
+FILES_ARRAY_LENGTH=${#FILES_ARRAY[@]}
+
+return_random() {
+    candidate=$((RANDOM % $FILES_ARRAY_LENGTH))
+    choice=${FILES_ARRAY[candidate]}
+}
+
+return_random
+
+while [ $choice = "." -o $choice = ".." ]
+    do
+        return_random
+    done
+
+echo "Trying: \"$choice\", $candidate of $FILES_ARRAY_LENGTH"
+`openvpn $1$choice`
