bc224bad056c0a6f16ffc5177526930ff1347313
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-05 12:25:11 +0000

    Create ssh_key_carver.sh

---
+++ shellscripts/ssh_key_carver.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+KEY_DIR=sshkeys;
+
+if [ ! -d "$KEY_DIR" ]; then
+    mkdir $KEY_DIR;
+    cd $KEY_DIR;
+else
+    echo "It appears the '$KEY_DIR' directory already exists. No further action. Quiting.";
+    exit;
+fi
+
+i=1;
+for f in $(grep -R "BEGIN RSA PRIVATE KEY" /home/* | cut -d':' -f1); do
+    cp $f ssh_key-$i;
+    i=$((i+1))
+done
