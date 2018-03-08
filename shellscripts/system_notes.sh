5c958d2d731657a15bc386e404a1a4a6b26c6bc8
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-03-08 20:57:22 +0000

    Update system_notes.sh

--- shellscripts/system_notes.sh
+++ shellscripts/system_notes.sh
@@ -7,9 +7,25 @@ echo "Assessment underway"
 
 HOST_NAME = $(hostname)
 FILE = "/etc/shadow"
+REMOTE = "www.google.com"
 
 echo "Script is running on ${HOST_NAME}."
 if[ -e "$FILE"]
 then
     echo "Shadow passwords are enabled on this system."
+fi
+
+if [ -w "$FILE"]
+then
+    echo "This account has permission to edit ${FILE}."
+else
+    echo "This account does NOT have permission to edit ${FILE}."
+fi
+
+ping -c 1 $HOST
+if[ "$?" -eq "0"]
+then
+    echo "$HOST is reachable."
+else
+    echo "$HOST is not reachable."
 fi
