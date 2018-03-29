f93ccf0387764f0a1377299e77032c0f106c954e
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-03-29 14:32:46 +0100

    Update ExamplePasswordGenerator.php

--- PHP/ExamplePasswordGenerator.php
+++ PHP/ExamplePasswordGenerator.php
@@ -98,3 +98,23 @@ public function generate()
 
 }
 ?>
+
+<?php
+define('CACHE_DIR', __DIR__ . '/cache');
+require __DIR__ . '/../Application/Autoload/Loader.php';
+Application\Autoload\Loader::init(__DIR__ . '/..');
+use Application\Security\PassGen;
+
+/* 
+define an array of websites that will be used as a source for the word-base to be used in password generation. In this illustration, we will choose from the Project Gutenberg texts Ulysses (J. Joyce), War and Peace (L. Tolstoy), and Pride and Prejudice (J. Austen)
+*/
+
+$source = [
+  'https://www.gutenberg.org/files/4300/4300-0.txt',
+  'https://www.gutenberg.org/files/2600/2600-h/2600-h.htm',
+  'https://www.gutenberg.org/files/1342/1342-h/1342-h.htm',
+];
+
+$passGen = new PassGen($source, 4, CACHE_DIR);
+echo $passGen->generate();
+?>
