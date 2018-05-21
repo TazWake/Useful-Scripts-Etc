96b8ec10b6e122a26fd11509d56375d48cd3a6b9
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-05-21 08:40:30 +0100

    Update timestamps.py

--- pythonscripts/timestamps.py
+++ pythonscripts/timestamps.py
@@ -3,7 +3,7 @@ import os.path
 
 for file in os.listdir("."):
     print file
-    print "Changed -> %s" % (time.ctime(os.path.getctime(file)))
+    print "Creation Time (Windows) -> %s" % (time.ctime(os.path.getctime(file)))
     print "Modified -> %s" % (time.ctime(os.path.getmtime(file)))
     print "Accessed -> %s" % (time.ctime(os.path.getatime(file)))
     print "\n"
