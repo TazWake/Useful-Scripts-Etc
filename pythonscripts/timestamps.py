f0543ac705d1a8c374130d9b54e3bbfc685af6a1
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-05-21 08:30:07 +0100

    Create timestamps.py

---
+++ pythonscripts/timestamps.py
@@ -0,0 +1,9 @@
+import time
+import os.path
+
+for file in os.listdir("."):
+    print file
+    print "Changed -> %s" % (time.ctime(os.path.getctime(file)))
+    print "Modified -> %s" % (time.ctime(os.path.getmtime(file)))
+    print "Accessed -> %s" % (time.ctime(os.path.getatime(file)))
+    print "\n"
