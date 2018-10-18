c5c40e623f7b0c1a44e68a754280b1f01c581f3e
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-10-18 15:12:49 +0100

    Update scapy_pcap.py

--- pythonscripts/scapy_pcap.py
+++ pythonscripts/scapy_pcap.py
@@ -8,13 +8,15 @@ packets = rdpcap('FILENAME.pcap')
 
 counter = 0
 
-# This version dumps the first 1000 packets out of a pcap in hex
+# This version prints the summary of each packet and the hex dump - for the first 1000 packets in a pcap
 
 for packet in packets:
     counter = counter + 1
     if counter =>0 and counter < 1001:
         hex_dump = ':'.join(x.encode('hex') for x in str(packet))
         print "### Packet : "+ str(counter)
+        print packet.summary()
+        print "\n"
         print hex_dump
         print "\n"
     if counter == 1001:
