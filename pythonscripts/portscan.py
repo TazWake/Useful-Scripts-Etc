5ae5180dca1a01b566d902e92016d4c298fedb6b
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-12-02 17:42:46 +0000

    Update portscan.py
    Initial commit

--- pythonscripts/portscan.py
+++ pythonscripts/portscan.py
@@ -3,3 +3,50 @@ from socket import *
 from threading import *
 
 screenLock = Semaphore(value=1)
+
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        connSkt.send('Peek-a-boo\r\n')
+        results = connSkt.recv(100)
+        screenLock.acquire()
+        print('[+]%d/tcp open'% tgtPort)
+        print('[+] ' + str(results))
+    except:
+        screenLock.acquire()
+        print('[-]%d/tcp closed'% tgtPort)
+    finally:
+        screenLock.release()
+        connSkt.close()
+
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print("[-] Cannot resolve '%s': Unknown host"%tgtHost)
+        return
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print('\n[+] Scan results for: ' + tgtName[0])
+    except:
+        print('\n[+] Scan results for: ' + tgtIP)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+
+def main():
+    parser = optparse.OptionParser('usage%prog '+\
+                                   '-H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string', help='Specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='Specify target port[s] separated by a comma.')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(',')
+    if(tgtHost == none) | (tgtPorts[0] == none):
+        print(parser.useage)
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+if __name__ == "__main__":
+    main()
