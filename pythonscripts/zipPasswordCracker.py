5199c0894158058282df9b7c7f8761ea709f06c1
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-12-02 18:15:04 +0000

    Update zipPasswordCracker.py

--- pythonscripts/zipPasswordCracker.py
+++ pythonscripts/zipPasswordCracker.py
@@ -1,2 +1,17 @@
 # Use to crack passwords on ZipFiles when other tools arent available
 import zipfile
+import optparse
+from threading import Thread
+
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        print('[+] Found password ' + password + '\n'
+    except:
+        pass
+
+def main():
+   parser = optparse.OptionParser("useage%prog " + "-f <zipfile> -d <dictionary>")
+   parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+   parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+   (options,args) = parser.parse_args()
