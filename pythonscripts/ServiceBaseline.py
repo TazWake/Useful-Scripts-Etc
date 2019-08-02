9f73972c1409b4185c9dd83415354b911a7472e3
Taz Wake <26778008+TazWake@users.noreply.github.com>
2019-08-02 23:28:09 +0100

    Update ServiceBaseline.py
    Added file storage

--- pythonscripts/ServiceBaseline.py
+++ pythonscripts/ServiceBaseline.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-
 # note - this script requires https://github.com/mhammond/pywin32
-
 from __future__ import print_function
 import pywintypes
 import win32api
 import win32con
-'''This script scans a clean (new build) system and establishes a baseline of known good paths and service binaries.'''
+import optparse
+
+'''
+This script scans a clean (new build) system and establishes a baseline of known good paths and service binaries.
+Useage: python ServiceBaseline.py [options]
+Options: -f <path and filename> | The path & filename you want the output saved
+'''
 
 def main():
+    parser = optparse.OptionParser('useage%prog -f <path and filename>')
+    parser.add_option('-f', dest='filename', type='string', help='Path and filename to where you want the output saved')
+    (options, args) = parser.parse_args()
+    f = options.filename
     read_perm = (win32con.KEY_READ | 
                  win32con.KEY_ENUMERATE_SUB_KEYS |
                  win32con.KEY_QUERY_VALUE)
@@ -24,6 +32,10 @@ def main():
         name = name.lower()
         path = path.lower()
         print(name, " = ", path)
+        f = open(f,wt)
+        f.write(name, " = ", path)
+        f.close
 
+        
 if __name__ == '__main__':
     main()
