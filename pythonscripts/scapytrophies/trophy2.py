5897ea46e4f232ad3437b3929d8bed1171f0d9d9
Taz Wake <26778008+TazWake@users.noreply.github.com>
2019-08-02 23:14:37 +0100

    Create trophy2.py
    This is the response to trophy 2 - https://github.com/guedou/GreHack17_scapy-handson/blob/master/trophies/network_interactions.md - currently a work in progress :-)

---
+++ pythonscripts/scapytrophies/trophy2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import optparse
+from scapy.all import *
+
+'''
+This is the script for Trophy 2
+'''
+
+def singleicmp(ipadd):
+    '''Send a single ICMP packet to a specified IP address'''
+    packet=IP(dst=ipadd,ttl=64)/ICMP()
+    pingu = sr1(packet)
+    return(pingu)
+
+def icmpframe(ipadd):
+    '''build an implicit ICMP frame (i.e. layer 2) with a TTL value from to 2 to 7 & send to the network etc'''
+    packet=IP(dst=ipadd,ttl=[2,7])/ICMP()
+    pingu = sr1(packet)
+    pingu.nsummary()
+    return()
+
+def main():
+    parser = optparse.OptionParser('usage%prog -i <IP Address to ping>')
+    parser.add_option('-i', dest='ipadd', type='string', help='The IP address you want to ping')
+    (options, args) = parser.parse_args()
+    ipadd = options.ipadd
+    a = singleicmp(ipadd)
+    print("\n-------ICMP ping sent----------\n")
+    print("-------The reply is:-----------\n")
+    a.show()
+    print("\n-------------------------------\n")
+
+
+if __name__ == "__main__":
+    main()
