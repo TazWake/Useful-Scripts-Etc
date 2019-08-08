ee9c130c7d05fb5a2b761967f77be7480a0fd21d
Taz Wake <26778008+TazWake@users.noreply.github.com>
2019-08-08 21:34:40 +0100

    Update ScapyPortScanner.py

--- pythonscripts/ScapyPortScanner.py
+++ pythonscripts/ScapyPortScanner.py
@@ -12,17 +12,16 @@ def main():
     parser.add_option('-i', dest='ipaddr', type='string', help='Specify the IP address you want to scan')
     parser.add_option('-p', dest='dports', type='string', help='Specify the port you want to scan - indivdual or comma separated list')
     (options, args) = parser.parse_args()
-    scan_dst = options.ipaddr
+    scan_dest = options.ipaddr
     scan_ports = options.dports
     if scan_ports == "":
         scan_ports = [21,22,80,139,443,445]
-    packet=IP(dst=scan_dst)/TCP(dport=scanports,flags="S")
+    packet=IP(dst=scan_dest,ttl=60)/TCP(dport=scanports,flags="S")
     responded, unanswered = sr(packet, timeout=10, verbose=0)
-    print("List of all open ports in "+scan_dst)
-    for a in responded:
-        if a[1][1].flags==18:
-            print a[1].sport
-
+    print("List of all open ports in "+scan_dest)
+    for x in responded:
+        if x[1][1].flags==18:
+            print x[1].sport
 
 if __name__ == "__main__":
     main()
